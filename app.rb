5b7d46ac080d4052085dbd65d27e321809dd879d
swlee-gameon <swlee@gameon.co.jp>
2016-05-11 18:46:59 +0900

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,5 +4,5 @@ require "sinatra"
 $stdout.sync = true
 
 get "/" do
-  "Hello from Flynn on port #{ENV["PORT"]} from container #{Socket.gethostname}\n"
+  "!234 Hello from Flynn on port #{ENV["PORT"]} from container #{Socket.gethostname}\n"
 end
